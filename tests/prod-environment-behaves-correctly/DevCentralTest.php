d820c641803e12dd0d89c80eb5db49a5147ddb26
Sébastien Santoro <dereckson@espace-win.org>
2016-07-11 03:11:55 +0000

    [Tests] DevCentral is now HTTPS only
    Summary:
    Test 301 redirect, fix http tests.
    
    Rel. T918
    
    Test Plan: make test
    
    Reviewers: dereckson
    
    Maniphest Tasks: T918
    
    Differential Revision: https://devcentral.nasqueron.org/D491

--- tests/prod-environment-behaves-correctly/DevCentralTest.php
+++ tests/prod-environment-behaves-correctly/DevCentralTest.php
@@ -6,10 +6,13 @@ class DevCentralTest extends PHPUnit_Framework_TestCase {
 	use assertHttp;
 
 	public function testWebsiteIsUp () {
-		$this->assertHttpResponseCode(200, 'http://devcentral.nasqueron.org', 'DevCentral looks down.');
 		$this->assertHttpResponseCode(200, 'https://devcentral.nasqueron.org', "DevCentral HTTPS issue.");
-		$this->assertHttpResponseCode(500, 'http://phabricator-files-for-devcentral-nasqueron.spacetechnology.net', "DevCentral alternative domain should return a 500 error code for homepage. Check phabricator.base-uri isn't empty.");
+		$this->assertHttpResponseCode(500, 'https://phabricator-files-for-devcentral-nasqueron.spacetechnology.net', "DevCentral alternative domain should return a 500 error code for homepage. Check phabricator.base-uri isn't empty.");
 	}
+
+    public function testNginxRedirectsHttpToHttps () {
+		$this->assertHttpResponseCode(301, 'http://devcentral.nasqueron.org', 'Nginx should redirect http to https with a 301 code.');
+    }
 
 	public function testAphlictIsUp () {
 		$this->assertHttpResponseCode(405, 'http://dwellers.nasqueron.org:22281/', 'Aphlict server seems down, does aphlict container is launched in Docker engine?');
