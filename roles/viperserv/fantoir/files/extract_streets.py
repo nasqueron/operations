6e11ea55d0d21eebb9c36b771aba21ea6449b81b
Sébastien Santoro <dereckson@espace-win.org>
2017-11-18 19:59:43 +0000

    Revert "Make the FANTOIR code more pythonic"
    Summary:
    This reverts commit 22090aeb3a7ed4e752d8429bbb66f6c9a392a417.
    
    Commit was wrongly marked as authored by @Dereckson by Arcanist,
    but it has been authored by @xcombelle.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Subscribers: xcombelle
    
    Differential Revision: https://devcentral.nasqueron.org/D1232

--- roles/viperserv/fantoir/files/extract_streets.py
+++ roles/viperserv/fantoir/files/extract_streets.py
@@ -9,28 +9,26 @@
 #   Data license:   FANTOIR is licensed under Licence Ouverte
 #   -------------------------------------------------------------
 
+
 import sys
 
 
 def extract_streets(filename_source, filename_out):
-    with open(filename_out, 'w') as output,\
-         open(filename_source, 'r') as input:
-        for line in input:
+    with open(filename_out, 'w') as fd:
+        for line in open(filename_source, 'r'):
             # Streets and other « voies » are the record where
             # the 109th position (« type de voie ») is 1.
-            try:
-                if line[108] == "1":
-                    output.write(line)
-            except IndexError:
-                pass
+            if len(line) < 109:
+                continue
+
+            if line[108] == "1":
+                fd.write(line)
 
 
-if __name__ == "__main__":
-    argc = len(sys.argv)
-    if (argc != 3):
-        print("Usage: {} <FANTOIR filename> <street filename>"
-              .format(sys.argv[0]),
-              file=sys.stderr)
-        sys.exit(1)
+argc = len(sys.argv)
+if (argc != 3):
+    print("Usage: {} <FANTOIR filename> <street filename>".format(sys.argv[0]),
+          file=sys.stderr)
+    sys.exit(1)
 
-    extract_streets(sys.argv[1], sys.argv[2])
+extract_streets(sys.argv[1], sys.argv[2])
