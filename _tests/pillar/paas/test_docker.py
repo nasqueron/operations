nasqueron/operations

commit 3c52ac6a07059328a5ade7ed54a2fab5de5e13ae

Provision exim container for Sentry
Summary:
References:
  - https://docs.sentry.io/server/installation/docker/
  - https://hub.docker.com/r/tianon/exim4/

Ref T1360

Test Plan: Deployed on Equatower

Reviewers: dereckson

Reviewed By: dereckson

Maniphest Tasks: T1360

Differential Revision: https://devcentral.nasqueron.org/D2000

pillar/paas/docker.sls
@@ -346,7 +346,8 @@ docker_containers:
     # The Sentry instance uses a Redis and a PostgreSQL instance,
     # declared above.
     exim:
-      sentry_smtp: {}
+      sentry_smtp:
+        host: mx.sentry.nasqueron.org
 
     sentry_worker:
       sentry_worker_1: &sentry_links

roles/paas-docker/containers/exim.sls
@@ -1,0 +1,68 @@
+#   -------------------------------------------------------------
+#   Salt — Provision Docker engine
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#   Project:        Nasqueron
+#   Created:        2018-11-10
+#   License:        Trivial work, not eligible to copyright
+#   -------------------------------------------------------------
+
+{% set has_selinux = salt['grains.get']('selinux:enabled', False) %}
+{% set containers = pillar['docker_containers'][grains['id']] %}
+
+{% for instance, container in containers['exim'].items() %}
+{% set image = salt['paas_docker.get_image']("tianon/exim4", container) %}
+
+#   -------------------------------------------------------------
+#   Data directory
+#
+#   Only required if you provide some hostname to the SMTP server
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+{% if 'host' in container %}
+
+/srv/exim/{{ instance }}:
+  file.directory:
+    - user: 999
+    - group: 999
+    - makedirs: True
+
+/srv/exim/{{ instance }}/mailname:
+  file.managed:
+    - contents: {{ container['host'] }}
+
+{% if has_selinux %}
+
+selinux_context_{{ instance }}_exim_data:
+  selinux.fcontext_policy_present:
+    - name: /srv/exim/{{ instance }}
+    - sel_type: container_file_t
+
+selinux_context_{{ instance }}_exim_data_applied:
+  selinux.fcontext_policy_applied:
+    - name: /srv/exim/{{ instance }}
+
+{% endif %}
+
+{% endif %}
+
+#   -------------------------------------------------------------
+#   Container
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+{{ instance }}:
+  docker_container.running:
+    - detach: True
+    - interactive: True
+    - image: {{ image }}
+
+{% if 'host' in container %}
+    - binds: /srv/exim/{{ instance }}/mailname:/etc/mailname:ro
+    - hostname: container['host']
+{% endif %}
+
+{% if 'network' in container %}
+    - networks:
+      - {{ container['network'] }}
+{% endif %}
+
+{% endfor %}

commit 24aa4870b750345d4268e4e3a8df2efb15bbf454

Check docker_container app_port/host
Summary:
If an entry under docker_containers has `host` defined, it needs also
to have `app_port` defined. The `host` key is used by nginx to determine
if a web configuration should be generated for this container.

This change offers an unit test to validate the pillar configuration,
and fixes the issue for exim container introduced in 3c52ac6a.

Test Plan: New config test provided

Reviewers: dereckson

Reviewed By: dereckson

Differential Revision: https://devcentral.nasqueron.org/D2007

_tests/Makefile
@@ -1,3 +1,3 @@
 test:
 	python -m unittest discover modules
-
+	python -m unittest discover pillar

pillar/paas/docker.sls
@@ -348,7 +348,7 @@ docker_containers:
     # declared above.
     exim:
       sentry_smtp:
-        host: mx.sentry.nasqueron.org
+        mailname: mx.sentry.nasqueron.org
 
     sentry:
       sentry_web_1:

roles/paas-docker/containers/exim.sls
@@ -28,7 +28,7 @@
 
 /srv/exim/{{ instance }}/mailname:
   file.managed:
-    - contents: {{ container['host'] }}
+    - contents: {{ container['mailname'] }}
 
 {% if has_selinux %}
 
@@ -57,7 +57,7 @@ selinux_context_{{ instance }}_exim_data_applied:
 
 {% if 'host' in container %}
     - binds: /srv/exim/{{ instance }}/mailname:/etc/mailname:ro
-    - hostname: container['host']
+    - hostname: container['mailname']
 {% endif %}
 
 {% if 'network' in container %}

_tests/pillar/paas/test_docker.py
@@ -1,0 +1,25 @@
+import unittest
+import yaml
+
+
+PILLAR_FILE = '../pillar/paas/docker.sls'
+
+
+class Testinstance(unittest.TestCase):
+
+    def setUp(self):
+        with open(PILLAR_FILE, 'r') as fd:
+            self.pillar = yaml.load(fd)
+
+    # nginx needs a host/app_port pair to spawn a configuration
+    def test_host_is_paired_with_app_port_option(self):
+        for node, services in self.pillar['docker_containers'].items():
+            for service, containers in services.items():
+                for instance, container in containers.items():
+                    if 'host' not in container:
+                        continue
+
+                    entry = ':'.join(['docker_containers', node,
+                                      service, instance])
+                    self.assertIn('app_port', container,
+                                  entry + ": app_port missing")