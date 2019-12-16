e90d3fcaec3d8eec596166780a13d0afe7f8b4fc
Sébastien Santoro <dereckson@espace-win.org>
2019-12-16 22:39:39 +0000

    Detect invalid or missing properties for shellusers
    Summary:
    This test has been added to detect a typo in user properties,
    like in D2115 "ssh-keys" instead of "ssh_keys".
    
    Test Plan: Tested with D2215 bogus and fixed versions.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Subscribers: alina-precup
    
    Differential Revision: https://devcentral.nasqueron.org/D2125

---
+++ _tests/pillar/core/test_users.py
@@ -0,0 +1,37 @@
+import unittest
+import yaml
+
+
+PILLAR_FILE = '../pillar/core/users.sls'
+
+USER_PROPERTIES_MANDATORY = set(["fullname", "ssh_keys", "uid"])
+USER_PROPERTIES_OPTIONAL = set([
+    "shell",
+    "yubico_keys",
+    "deploy_dotfiles_to_devserver"
+])
+
+
+class Testinstance(unittest.TestCase):
+
+    def setUp(self):
+        with open(PILLAR_FILE, 'r') as fd:
+            self.pillar = yaml.safe_load(fd)
+
+    # users must have an username, an UID and SSH keys
+    def test_users_properties(self):
+        is_valid = True
+        errors = []
+
+        for user, properties in self.pillar["shellusers"].items():
+            missing_properties = USER_PROPERTIES_MANDATORY - set(properties)
+            if missing_properties:
+                errors.append(f"  Missing properties for {user}: {missing_properties}")
+                is_valid = False
+
+            invalid_properties = set(properties) - USER_PROPERTIES_MANDATORY - USER_PROPERTIES_OPTIONAL
+            if invalid_properties:
+                errors.append(f"  Invalid properties for {user}: {invalid_properties}")
+                is_valid = False
+
+        self.assertTrue(is_valid, "\n" + "\n".join(errors))
