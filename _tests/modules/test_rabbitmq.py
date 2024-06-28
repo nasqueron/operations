c3638baac09cb9671d0146c006a460e55ece14e2
Sébastien Santoro <dereckson@espace-win.org>
2024-06-28 17:54:45 +0000

    Fix tests for RabbitMQ password hash
    Summary:
    This is a follow-up for 79c7da764e62.
    
    Ref T752.
    
    Test Plan: `python3 -m unittest modules/test_rabbitmq.py`
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Maniphest Tasks: T752
    
    Differential Revision: https://devcentral.nasqueron.org/D3347

--- _tests/modules/test_rabbitmq.py
+++ _tests/modules/test_rabbitmq.py
@@ -5,12 +5,12 @@ import unittest
 
 
 salt_test_case = SourceFileLoader("salt_test_case", "salt_test_case.py").load_module()
-rabbitmq = SourceFileLoader("rabbitmq", "../_modules/rabbitmq.py").load_module()
+rabbitmq = SourceFileLoader("rabbitmq", "../_modules/rabbitmq_api.py").load_module()
 
 
 class Testinstance(unittest.TestCase, salt_test_case.SaltTestCase):
     def test_compute_password_hash_with_salt(self):
         self.assertEqual(
             "kI3GCqW5JLMJa4iX1lo7X4D6XbYqlLgxIs30+P6tENUV2POR",
-            rabbitmq._compute_password_hash_with_salt(0x908DC60A, "test12"),
+            rabbitmq.compute_password_hash_with_salt(0x908DC60A, "test12"),
         )
