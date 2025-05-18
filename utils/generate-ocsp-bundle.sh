eb1c41a8d8ef5356b96cbbfd878faf5071b2f0ef
Sébastien Santoro <dereckson@espace-win.org>
2025-05-18 09:18:41 +0000

    Update Let's Encrypt intermediate certificates for OCSP
    Summary: Pending T2114, we can update the list of currently active certificates.
    
    Test Plan: `nginx -t`
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Subscribers: DorianWinty
    
    Differential Revision: https://devcentral.nasqueron.org/D3636

--- utils/generate-ocsp-bundle.sh
+++ utils/generate-ocsp-bundle.sh
@@ -11,19 +11,31 @@
 #   Let's encrypt
 #
 #   Active certificates:
-#     - Let’s Encrypt R3 - signed by ISRG Root X1
-#     - Let’s Encrypt E1 - signed by ISRG Root X2
+#     - Let’s Encrypt R10 - signed by ISRG Root X1
+#     - Let’s Encrypt R11 - signed by ISRG Root X1
+#     - Let’s Encrypt E5  - signed by ISRG Root X1 and X2
+#     - Let’s Encrypt E6  - signed by ISRG Root X1 and X2
 #
-#   Disaster recovery certificates:
-#     - Let’s Encrypt R4 - signed by ISRG Root X1
-#     - Let’s Encrypt E2 - signed by ISRG Root X2
+#   Backup certificates:
+#     - Let’s Encrypt R12 - signed by ISRG Root X1
+#     - Let’s Encrypt R13 - signed by ISRG Root X1
+#     - Let’s Encrypt R14 - signed by ISRG Root X1
+#     - Let’s Encrypt E7  - signed by ISRG Root X1 and X2
+#     - Let’s Encrypt E8  - signed by ISRG Root X1 and X2
+#     - Let’s Encrypt E9  - signed by ISRG Root X1 and X2
 #   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-curl -sS https://letsencrypt.org/certs/lets-encrypt-r3.pem
-curl -sS https://letsencrypt.org/certs/lets-encrypt-e1.pem
+curl -sS https://letsencrypt.org/certs/2024/r10.pem
+curl -sS https://letsencrypt.org/certs/2024/r11.pem
+curl -sS https://letsencrypt.org/certs/2024/r12.pem
+curl -sS https://letsencrypt.org/certs/2024/r13.pem
+curl -sS https://letsencrypt.org/certs/2024/r14.pem
 
-curl -sS https://letsencrypt.org/certs/lets-encrypt-r4.pem
-curl -sS https://letsencrypt.org/certs/lets-encrypt-e2.pem
+curl -sS https://letsencrypt.org/certs/2024/e5.pem
+curl -sS https://letsencrypt.org/certs/2024/e6.pem
+curl -sS https://letsencrypt.org/certs/2024/e7.pem
+curl -sS https://letsencrypt.org/certs/2024/e8.pem
+curl -sS https://letsencrypt.org/certs/2024/e9.pem
 
 curl -sS https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem
 curl -sS https://letsencrypt.org/certs/isrg-root-x2-cross-signed.pem
