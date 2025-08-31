60c7955e1c0e4bf553e3e175b903569ea632ec4f
Sébastien Santoro <dereckson@espace-win.org>
2025-08-31 15:59:38 +0000

    Allow to discover IPv6 gateway for Online servers
    Summary:
    Online doesn't document gateway for IPv6 blocks.
    
    Normally, `inet6 -ifdisabled accept_rtadv up` and the DHCPv6 request
    to authenticate the right to use the block with DUID should be enough.
    
    If we want to explicitly set a gateway in the routing table, we can
    discover it through neighbour discovery protocol (NDP): this will be
    the only IP adress not associated to a known MAC address.
    
    With that informatino, gateway can then be declared on NetBox
    by an ops running the query-ndp-for-ipv6-neighbor.sh script.
    
    Test Plan: `sh utils/query-ndp-for-ipv6-neighbor.sh` on WindRiver
    
    Reviewers: DorianWinty
    
    Reviewed By: DorianWinty
    
    Differential Revision: https://devcentral.nasqueron.org/D3651

---
+++ utils/query-ndp-for-ipv6-neighbor.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# shellcheck disable=SC2086
+
+#   -------------------------------------------------------------
+#   rOPS — determine IPv6 router with neighbor discovery protocol
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#   Project:        Nasqueron
+#   Description:    Read the output of `ndp -an` to find the first
+#                   IP address not matching one of the server MAC.
+#                   That should be a de facto gateway.
+#   -------------------------------------------------------------
+
+set -e
+
+#   -------------------------------------------------------------
+#   Parse arguments
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <interface>"
+    echo "Example: $0 igb0"
+    exit 1
+fi
+
+IFACE="$1"
+
+#   -------------------------------------------------------------
+#   Collect local MAC addresses
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+LOCAL_MACS=$(ifconfig "$IFACE" | while read -r line; do
+    case "$line" in
+        *ether*) set -- $line; echo "$2";;
+    esac
+done)
+
+LOCAL_MACS_STR=""
+for m in $LOCAL_MACS; do
+    LOCAL_MACS_STR="$LOCAL_MACS_STR $m"
+done
+
+#   -------------------------------------------------------------
+#   Parse ndp -an output
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+skip=1
+ndp -an | while read -r ipv mac rest; do
+    # Skip header or incomplete entrie
+    if [ $skip -eq 1 ]; then skip=0; continue; fi
+    [ "$mac" = "(incomplete)" ] && continue
+
+    # Skip local MACs
+    found=0
+    for m in $LOCAL_MACS_STR; do
+        [ "$mac" = "$m" ] && found=1 && break
+    done
+    [ $found -eq 1 ] && continue
+
+    # We've got a winner!
+    case "$ipv" in
+        *%*) ipv=${ipv%%%*} ;;
+    esac
+
+    echo "$ipv"
+    break
+done
