3bae0bccf2aaa304b8d0cb60f016357aae54760c
Sébastien Santoro <dereckson@espace-win.org>
2025-09-11 22:54:48 +0000

    Fix docker_containers app_port/host test
    Summary:
    Instead of reading the pillar from paas/docker.sls,
    with Salt Tower, we need to read every .sls files
    in pillar/paas/docker/.
    
    The new structure contains directly the services,
    so we skip the "by node" step when reading the dictionary.
    
    This is a follow-up for bbc48f9eb26d.
    
    Ref T2123
    
    Test Plan: `(cd _tests && python3 -m unittest pillar/paas/test_docker.py)`
    
    Reviewers: DorianWinty
    
    Maniphest Tasks: T2123
    
    Differential Revision: https://devcentral.nasqueron.org/D3660

--- _tests/pillar/paas/test_docker.py
+++ _tests/pillar/paas/test_docker.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 
+from unittest_data_provider import data_provider
+import os
 import unittest
 import yaml
 
 
-PILLAR_FILE = "../pillar/paas/docker.sls"
+PILLAR_PATH = "../pillar/paas/docker/"
 
 
 class Testinstance(unittest.TestCase):
-    def setUp(self):
-        with open(PILLAR_FILE, "r") as fd:
-            self.pillar = yaml.safe_load(fd)
+    def pillar_files():
+        sls_files = find_sls_files(PILLAR_PATH)
+
+        return tuple((file,) for file in sls_files)
 
     # nginx needs a host/app_port pair to spawn a configuration
-    def test_host_is_paired_with_app_port_option(self):
-        for node, services in self.pillar["docker_containers"].items():
-            for service, containers in services.items():
-                for instance, container in containers.items():
-                    if "host" not in container:
-                        continue
+    @data_provider(pillar_files)
+    def test_host_is_paired_with_app_port_option(self, pillar_file):
+        with open(pillar_file, "r") as fd:
+            self.pillar = yaml.safe_load(fd)
 
-                    entry = ":".join(["docker_containers", node, service, instance])
-                    self.assertIn("app_port", container, entry + ": app_port missing")
+        for service, containers in self.pillar["docker_containers"].items():
+            for instance, container in containers.items():
+                if "host" not in container:
+                    continue
+
+                entry = ":".join(["docker_containers", pillar_file, service, instance])
+                self.assertIn("app_port", container, entry + ": app_port missing")
+
+
+def find_sls_files(path):
+    sls_files = []
+
+    for root, _, files in os.walk(path):
+        for filename in files:
+            if filename.endswith(".sls"):
+                sls_files.append(os.path.join(root, filename))
+
+    return sls_files
 
 
 if __name__ == "__main__":
