7f3fde8da8148575372292fa8a20d4f6375c8583
Sébastien Santoro <dereckson@espace-win.org>
2026-02-28 16:55:05 +0000

    Help to install Arcanist dependencies
    This is a follow-up for 5b60dd5b.
    
    Reviewed By: Duranzed
    
    Differential Revision: https://devcentral.nasqueron.org/D3984

--- utils/install-arc.sh
+++ utils/install-arc.sh
@@ -13,6 +13,35 @@ ARC_DIR=/opt/phabricator
 BIN_DIR=/usr/local/bin
 
 #   -------------------------------------------------------------
+#   Dependencies
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_dependencies()
+{
+    if [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ]; then
+        apt install git php php-curl php-dom shellcheck
+    elif [ "$ID" = "freebsd" ]; then
+        echo pkg install git php83 php83-curl php83-dom hs-ShellCheck
+    elif [ "$ID" = "fedora" ]; then
+        dnf install git php php-dom shellcheck
+    elif [ "$ID" = "darwin" ]; then
+        brew install git php shellcheck
+    else
+        echo "Can't detect OS, add a condition or a new block in install_dependencies. ID: '$ID'."
+    fi
+}
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+elif command -v brew; then
+    # macOS doesn't use os-release, but we can still detect Homebrew
+    ID=darwin
+fi
+install_dependencies
+
+exit 0
+
+#   -------------------------------------------------------------
 #   Clone repositories
 #   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
