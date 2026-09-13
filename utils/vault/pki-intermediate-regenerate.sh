28ada36c6cf404435628c4ea8e2a91111833f9e8
Sébastien Santoro <dereckson@espace-win.org>
2026-09-13 23:25:35 +0000

    Renew intermediate CA certificate with vault pki reissue
    Shortcut compatible with vault 1.12+.
    
    Ref T1966
    
    Test Plan: renewed pki_vault for 1 year 2026-09-13
    
    Differential Revision: https://devcentral.nasqueron.org/D4216

--- utils/vault/pki-intermediate-regenerate.sh
+++ utils/vault/pki-intermediate-regenerate.sh
@@ -1,36 +1,56 @@
 #!/bin/sh
 
-set -e
+#   -------------------------------------------------------------
+#   Renew Vault intermediate CA :: pki_vault
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#   Project:        Nasqueron
+#   License:        BSD-2-Clause
+#   -------------------------------------------------------------
 
-export VAULT_SKIP_VERIFY=1
+set -e
 
 PKI_ROOT=pki_root
 PKI_INTERMEDIATE=pki_vault
-WORKDIR=$(mktemp -d -t vault-intermediate)
+ROLE=nasqueron-drake
+VAULT=vault
 
-cd "$WORKDIR"
+#   -------------------------------------------------------------
+#   Issuers
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-vault write -format=json $PKI_INTERMEDIATE/intermediate/generate/internal \
+# Root CA issuer (from DRP bootstrap script: root-<YEAR>, but not yet active)
+ROOT_ISSUER=default
+
+# Intermediate CA issuer
+ROLE_JSON=$($VAULT read -tls-skip-verify -format=json $PKI_INTERMEDIATE/roles/$ROLE)
+CURRENT_ISSUER=$(echo "$ROLE_JSON" | jq -r .data.issuer_ref)
+NEW_ISSUER="drake-nasqueron-intermediate-$(date +%Y%m%d)"
+
+#   -------------------------------------------------------------
+#   Reissue the intermediate certificate
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+$VAULT pki reissue -tls-skip-verify \
+    -issuer_name="$NEW_ISSUER" \
+    "/$PKI_ROOT/issuer/$ROOT_ISSUER" \
+    "/$PKI_INTERMEDIATE/issuer/$CURRENT_ISSUER" \
+    "/$PKI_INTERMEDIATE/" \
     common_name="nasqueron.drake Intermediate Authority" \
-    issuer_name="drake-nasqueron-intermediate" \
-    | jq -r '.data.csr' > cert_intermediate.csr
+    organization="Nasqueron" \
+    ou="Nasqueron Operations SIG" \
+    country="BE" \
+    ttl="8760h"
 
-vault write -format=json $PKI_ROOT/root/sign-intermediate \
-    issuer_ref="root-2022" \
-    csr=@cert_intermediate.csr \
-    format=pem_bundle ttl="8760h" \
-    | jq -r '.data.certificate' > cert_intermediate.pem
+#   -------------------------------------------------------------
+#   Update role with new issuer
+#   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-ISSUER=$(vault write -format=json $PKI_INTERMEDIATE/intermediate/set-signed \
-    certificate=@cert_intermediate.pem \
-    | jq -r '.data.imported_issuers[0]')
+role_file=$(mktemp /tmp/role.old.XXXX)
+updated_role_file=$(mktemp /tmp/role.new.XXXX)
+trap 'rm -f "$role_file" "$updated_role_file"' EXIT
 
-vault write $PKI_INTERMEDIATE/roles/nasqueron-drake \
-    issuer_ref="$ISSUER" \
-    allowed_domains="nasqueron.drake" \
-    allow_subdomains=true \
-    max_ttl="2160h"
+$VAULT read -tls-skip-verify -format=json $PKI_INTERMEDIATE/roles/$ROLE > "$role_file"
+jq --arg issuer "$NEW_ISSUER" '.data.issuer_ref = $issuer | .data' "$role_file" > "$updated_role_file"
+$VAULT write -tls-skip-verify $PKI_INTERMEDIATE/roles/$ROLE @"$updated_role_file"
 
-cd /tmp
-rm "$WORKDIR"/cert*
-rmdir "$WORKDIR"
+echo "✅ Intermediate CA renewed and role '$ROLE' updated to use '$NEW_ISSUER'."
